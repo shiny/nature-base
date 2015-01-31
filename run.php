e0c01b38a55d95c1629a43aee364bc7e56f34979
Shiny <daijie@php.net>
2015-01-31 18:00:54 +0800

    class name change to App

--- run.php
+++ run.php
@@ -3,7 +3,7 @@ if(isset($app) && is_callable(array($app, 'run'))) {
     $app->run();
 } else {
     foreach (get_defined_vars() as $var) {
-        if(is_a($var, 'nature') && is_callable(array($var, 'run'))) {
+        if(is_a($var, 'Nature\App') && is_callable(array($var, 'run'))) {
             $var->run();
         }
     }
